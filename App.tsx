2c53a011ec2223ad9ad3bbfa8b24223edf901b04
Tuomas Suutari <tuomas@nepnep.net>
2023-05-29 11:30:35 +0300

    Järjestä importit

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React, {useState} from 'react';
 import {
     Button,
@@ -7,8 +9,6 @@ import {
     Text,
     View,
 } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
