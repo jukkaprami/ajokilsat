40d488db6b383e9f635ec5a61b5d73885ba85e45
Tuomas Suutari <tuomas@nepnep.net>
2023-05-29 11:22:19 +0300

    Navigaatiotesti

--- App.tsx
+++ App.tsx
@@ -1,47 +1,84 @@
 import React, {useState} from 'react';
 import {
-  Button,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
+    Button,
+    SafeAreaView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    View,
 } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [hidden, setHidden] = useState(false);
-  const changeStatusBarVisibility = () => setHidden(!hidden);
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="home"
+                    component={ButtonOnlyView}
+                    options={{title: 'Aloitusruutu'}}
+                />
+                <Stack.Screen
+                    name="other"
+                    component={StatusBarTogglerView}
+                    options={{title: 'Toinen juttu'}}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar
-        animated={true}
-        backgroundColor="#61dafb"
-        barStyle={'default'}
-        hidden={hidden}
-      />
-      <Text style={styles.textStyle}>
-        StatusBar Visibility:{'\n'}
-        {hidden ? 'Hidden' : 'Visible'}
-      </Text>
-      <View style={styles.buttonsContainer}>
-        <Button title="Toggle StatusBar" onPress={changeStatusBarVisibility} />
-      </View>
-    </SafeAreaView>
-  );
-};
+function ButtonOnlyView({navigation}) {
+    return (
+        <View>
+            <Button
+                title="Avaa juttu"
+                onPress={() => navigation.navigate('other')}
+            />
+        </View>
+    );
+}
+
+function StatusBarTogglerView() {
+    const [hidden, setHidden] = useState(false);
+    const changeStatusBarVisibility = () => setHidden(!hidden);
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar
+                animated={true}
+                backgroundColor="#61dafb"
+                barStyle={'default'}
+                hidden={hidden}
+            />
+            <Text style={styles.textStyle}>
+                StatusBar Visibility:{'\n'}
+                {hidden ? 'Hidden' : 'Visible'}
+            </Text>
+            <View style={styles.buttonsContainer}>
+                <Button
+                    title="Toggle StatusBar"
+                    onPress={changeStatusBarVisibility}
+                />
+            </View>
+        </SafeAreaView>
+    );
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ECF0F1',
-  },
-  buttonsContainer: {
-    padding: 10,
-  },
-  textStyle: {
-    textAlign: 'center',
-    marginBottom: 8,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#ECF0F1',
+    },
+    buttonsContainer: {
+        padding: 10,
+    },
+    textStyle: {
+        textAlign: 'center',
+        marginBottom: 8,
+    },
 });
