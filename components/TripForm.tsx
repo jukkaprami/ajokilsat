364ec2132814830756d4509e6814ddebc01ad8b7
Tuomas Suutari <tuomas@nepnep.net>
2023-08-21 16:00:38 +0300

    Lisää aloitusajan valinta TripForm:lle

--- components/TripForm.tsx
+++ components/TripForm.tsx
@@ -1,25 +1,39 @@
+import {DateTimePickerAndroid} from '@react-native-community/datetimepicker';
+import * as React from 'react';
 import {View} from 'react-native';
-import {Text} from 'react-native-paper';
+import {Button, SegmentedButtons, Text, TextInput} from 'react-native-paper';
 
 export default function TripForm() {
+    const [vehicle, setVehicle] = React.useState('car1');
+    const [startDate, setStartDate] = React.useState(new Date());
+
+    const setStartDateMode = (mode: 'date' | 'time') => {
+        DateTimePickerAndroid.open({
+            value: startDate,
+            onChange: (event: any, x: Date) => {
+                setStartDate(x);
+                if (mode == 'date') setStartDateMode('time');
+            },
+            mode,
+            is24Hour: true,
+        });
+    };
+    const showStartDatePicker = () => setStartDateMode('date');
+
     return (
         <View>
-            <Text>Matka-lomake</Text>
+            <SegmentedButtons
+                value={vehicle}
+                onValueChange={setVehicle}
+                buttons={[
+                    {value: 'car1', label: 'Auto 1'},
+                    {value: 'car2', label: 'Auto 2'},
+                ]}
+            />
+            <TextInput label="Ajon kuvaus" />
+            <Text>Alku: {startDate.toLocaleString()}</Text>
+            <Button onPress={showStartDatePicker}>Valitse alkuaika</Button>
+            <Button mode="contained">Tallenna</Button>
         </View>
     );
 }
-
-/*
-function StatusBarTogglerView() {
-    return (
-        <View style={styles.container}>
-            <StatusBar {...styles.statusBar} />
-            <View style={styles.buttonsContainer}>
-                <TextInput label="Ajon kuvaus" />
-                <Button mode="contained">Aloita ajo</Button>
-            </View>
-        </View>
-    );
-}
-
-*/
