70d2a015968ba98c122dcbc73dbab08300fceac6
Tuomas Suutari <tuomas@nepnep.net>
2023-09-04 10:30:39 +0300

    Tee Trip-tyyin timestamp-kentistä vapaaehtoisia

--- types/Trip.tsx
+++ types/Trip.tsx
@@ -1,8 +1,8 @@
 export type Trip = {
     vehicleId: string;
     description: string;
-    timestampAtBegin: Date | null;
-    timestampAtEnd: Date | null;
+    timestampAtBegin?: Date | null;
+    timestampAtEnd?: Date | null;
     odometerAtBegin?: number | null;
     odometerAtEnd?: number | null;
     routeDescription?: string;
