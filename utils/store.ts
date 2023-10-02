aec0211730dc572fc42d593f6e0ea620eea7a602
Jukka Prami <43521@edu.raseko.fi>
2023-10-02 09:24:40 +0300

    1

--- utils/store.ts
+++ utils/store.ts
@@ -66,26 +66,3 @@ async function saveTripsToFile(trips: Trip[]) {
     savedTrips = [...trips];
     await overwriteJsonFile(TRIPS_FILE, savedTrips);
 }
-
-const exampleTrips: Trip[] = [
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Työmatka',
-        timestampAtBegin: new Date(2023, 8, 18, 8, 15),
-        timestampAtEnd: new Date(2023, 8, 18, 9, 15),
-        routeDescription: 'Salo - Helsinki',
-        odometerAtBegin: 125052,
-        odometerAtEnd: 125118,
-    },
-    {
-        id: newId(),
-        vehicleId: 'car2',
-        description: 'Visiitti Ahvenanmaalle',
-        timestampAtBegin: new Date(2023, 8, 15, 10, 45),
-        timestampAtEnd: new Date(2023, 8, 15, 22, 35),
-        routeDescription: 'Tampere - Turku - Laiva - Ahvenanmaa',
-        odometerAtBegin: 125130,
-        odometerAtEnd: 125445,
-    },
-];
